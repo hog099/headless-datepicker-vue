9ad122c49a48a3ddc15946762312831b3fca2315
Hudson O Ginelli <oliveiraginelli@gmail.com>
2026-05-07 17:48:52 -0300

    feat: commit lint config

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 150],
+    "scope-case": [2, "never", ["kebab-case"]],
+    "subject-case": [2, "never", ["pascal-case", "upper-case"]],
+    "type-case": [2, "always", "lower-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "chore",
+        "docs",
+        "feat",
+        "fix",
+        "wip",
+        "merge",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "hotfix",
+      ],
+    ],
+  },
+};
