8be7578de17514221488657a77d0e5620a3a242e
Hudson O Ginelli <oliveiraginelli@gmail.com>
2026-05-08 08:01:07 -0300

    fix: adjust vite config base with name repo correctly

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/headless-datepicker/",
+  base: "/headless-datepicker-vue/",
   server: {
     port: 3004,
   },
